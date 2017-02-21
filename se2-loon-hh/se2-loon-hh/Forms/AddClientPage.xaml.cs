757b1b2ff38bac2992432d439ba0a2e9bed236d0
Danny Morrow <morrowdt@etsu.edu>
2017-02-21 03:00:30 -0500

    Changed Birthday from textboxes to DatePicker. Added fields for partner, pregnancy info and history, and children.

--- se2-loon-hh/se2-loon-hh/Forms/AddClientPage.xaml.cs
+++ se2-loon-hh/se2-loon-hh/Forms/AddClientPage.xaml.cs
@@ -22,10 +22,13 @@ namespace se2_loon_hh.Forms
     public partial class AddClientPage : Page
     {
         ClientController clientController;
+        private int childCounter;
+
         public AddClientPage()
         {
             clientController = new ClientController();
             InitializeComponent();
+            childCounter = 0;
             this.DataContext = this; // This is for allowing the height and width of the page to be bound to the window height and width
         }
 
@@ -37,6 +40,62 @@ namespace se2_loon_hh.Forms
         private void SubmitButton_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Placeholder Text");
+        }
+
+        private void AddChildButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Will need to put controller code in here for creating children later
+            // TODO: Make children deletable
+            childCounter++;
+
+            GroupBox groupbox = new GroupBox(); // Surrounds all elements for child
+            groupbox.Header = "Child " + childCounter;
+
+            StackPanel childPanel = new StackPanel(); // Layout for elements
+
+            Grid fnameGrid = new Grid(); // Layout for fname elements
+            Label fnameLabel = new Label();
+            fnameLabel.Content = "First Name:";
+            TextBox fnameTextbox = new TextBox();
+
+            Grid lnameGrid = new Grid(); // Layout for lname elements
+            Label lnameLabel = new Label();
+            lnameLabel.Content = "Last Name:";
+            TextBox lnameTextbox = new TextBox();
+
+            Grid bdayGrid = new Grid(); // Layout for bday elements
+            Label bdayLabel = new Label();
+            bdayLabel.Content = "Birthdate:";
+            DatePicker bdayPicker = new DatePicker();
+
+            Grid custodyGrid = new Grid(); // Layout for custody elements
+            Label custodyLabel = new Label();
+            custodyLabel.Content = "Custody:";
+            StackPanel custodyChoicesLayout = new StackPanel(); // Layout for yes/no radio buttons
+            custodyChoicesLayout.HorizontalAlignment = HorizontalAlignment.Right;
+            custodyChoicesLayout.Orientation = Orientation.Horizontal;
+            RadioButton yesButton = new RadioButton();
+            yesButton.Content = "Yes";
+            RadioButton noButton = new RadioButton();
+            noButton.Content = "No";
+
+            // Put all elements in corresponding layouts
+            custodyChoicesLayout.Children.Add(yesButton);
+            custodyChoicesLayout.Children.Add(noButton);
+            custodyGrid.Children.Add(custodyLabel);
+            custodyGrid.Children.Add(custodyChoicesLayout);
+            bdayGrid.Children.Add(bdayLabel);
+            bdayGrid.Children.Add(bdayPicker);
+            lnameGrid.Children.Add(lnameLabel);
+            lnameGrid.Children.Add(lnameTextbox);
+            fnameGrid.Children.Add(fnameLabel);
+            fnameGrid.Children.Add(fnameTextbox);
+            childPanel.Children.Add(fnameGrid);
+            childPanel.Children.Add(lnameGrid);
+            childPanel.Children.Add(bdayGrid);
+            childPanel.Children.Add(custodyGrid);
+            groupbox.Content = childPanel;
+            ChildContainer.Children.Insert(childCounter - 1, groupbox); // Inserts after last child, but before AddChildButton
         }
     }
 }
