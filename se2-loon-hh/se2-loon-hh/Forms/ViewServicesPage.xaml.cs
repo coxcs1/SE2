bb57e80c33a6b2b4837beb4d9e5d7905798334a1
lightdj <LIGHTDJ@etsu.edu>
2017-03-23 13:42:50 -0400

    Added edit functionality for services. I just reused the add service page. Removed temp additions to the main page for access to my functionality.

--- se2-loon-hh/se2-loon-hh/Forms/ViewServicesPage.xaml.cs
+++ se2-loon-hh/se2-loon-hh/Forms/ViewServicesPage.xaml.cs
@@ -29,18 +29,39 @@ namespace se2_loon_hh.Forms
             InitializeComponent();
             setupComponents();
         }
-
+        /// <summary>
+        /// This function goes back to the home page.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
             this.NavigationService.Navigate(new MainPage());//go to home page
         }
-
+        /// <summary>
+        /// This function passes the service requested ID over to the 
+        /// service details page to display all information related to a service.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ServiceDetails_Click(object sender, RoutedEventArgs e)
         {
             var service = ServicesDataGrid.Items.GetItemAt(ServicesDataGrid.SelectedIndex);//fetch the service row that was clicked
             var serviceRequestedID = Convert.ToInt32(service.GetType().GetProperty("ServiceID").GetValue(service).ToString());//fetch the serviceID from the generic object
             //navigate to service detail page
             this.NavigationService.Navigate(new ServiceDetails(serviceRequestedID));
+        }
+        /// <summary>
+        /// This function navigates to the add service page and passes
+        /// the ID of the service requested to populate that form for editing.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void EditService_Click(object sender, RoutedEventArgs e)
+        {
+            var service = ServicesDataGrid.Items.GetItemAt(ServicesDataGrid.SelectedIndex);//fetch the service row that was clicked
+            var serviceRequestedID = Convert.ToInt32(service.GetType().GetProperty("ServiceID").GetValue(service).ToString());//fetch the serviceID from the generic object
+            this.NavigationService.Navigate(new AddServicePage(serviceRequestedID));
         }
 
         /// <summary>
