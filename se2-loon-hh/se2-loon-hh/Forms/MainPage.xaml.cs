6dd42fbcbf043a0552fc44c070953d69c7936edd
Zepton2 <Rslcwg@live.com>
2017-03-22 20:10:32 -0400

    Created a function in the ServiceController that returns a generic list of information for the view services page data grid. Created a service details page that will pull a specific service and show all the details about that service including the donations.

--- se2-loon-hh/se2-loon-hh/Forms/MainPage.xaml.cs
+++ se2-loon-hh/se2-loon-hh/Forms/MainPage.xaml.cs
@@ -41,5 +41,10 @@ namespace se2_loon_hh.Forms
         {
             this.NavigationService.Navigate(new AddServicePage());
         }
+
+        private void ViewServices_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new ViewServicesPage());
+        }
     }
 }
