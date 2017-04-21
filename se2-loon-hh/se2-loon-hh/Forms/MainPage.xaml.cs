5aaf5ee0b60e2216f86c0935f6d6ebc2627b91d3
crystal cox <coxcs1@etsu.edu>
2017-04-21 02:44:45 +0000

    MainPage.xaml.cs edited online with Bitbucket

--- se2-loon-hh/se2-loon-hh/Forms/MainPage.xaml.cs
+++ se2-loon-hh/se2-loon-hh/Forms/MainPage.xaml.cs
@@ -54,14 +54,29 @@ namespace se2_loon_hh.Forms
             this.NavigationService.Navigate(new AddClientPage());
         }
 
+        private void AddServiceMenu_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new AddServicePage());
+        }
+        
         private void ViewClientsMenu_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new ClientListPage());
+        }
+        
+        private void ViewServiceMenu_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new ViewServicesPage());
         }
 
         private void ViewServices_Click(object sender, RoutedEventArgs e)
         {
             this.NavigationService.Navigate(new ViewServicesPage());
+        }
+
+        private void AddEmergencyMenu_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new AddEmergency());
         }
     }
 }
